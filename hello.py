444bebada97336ca6d43512c73400d6a2eca1895
Nitin Raj <nitinrana9060888018@gmail.com>
2026-08-14 04:20:12 +0530

    basics

--- hello.py
+++ hello.py
@@ -1,1 +1,11 @@
+#basics about python
+# python is procedural langiage
+#.          object oriented 
+
+# flask vs django just remember django is for full stack development or for large application
+
+# flask is for mico framework
+
+# one important things python is indentation
+#indentation means we have to keep the block of code in correct block
 print("Hello ,world")
