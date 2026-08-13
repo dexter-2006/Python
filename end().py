17aec4bc1b36422bdb533381026372dacd80a404
Nitin Raj <nitinrana9060888018@gmail.com>
2026-08-14 04:24:37 +0530

    end()

---
+++ end().py
@@ -0,0 +1,10 @@
+# the statement in python ends with when line ends
+
+# we dont use semicolon ;
+
+#if we want to print multiple word in single line we have to use
+
+# end() parameter
+
+print("Hello World! ",end="")
+print("I will print on the same line")
