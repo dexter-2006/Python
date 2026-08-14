5c42780edd5ab8068cb98e5ca9a5fdd6e3a602cc
Nitin Raj <nitinrana9060888018@gmail.com>
2026-08-15 00:56:01 +0530

    multiple variable

---
+++ multiple_variable.py
@@ -0,0 +1,31 @@
+# value of multiple variable at the same time
+
+x,y,z ="orange","banana","cherry"
+
+print(x)
+print(y)
+print(z)
+
+# one value to multiple variables
+
+a=b=c="hang"
+
+print(a)
+print(b)
+print(c)
+
+# to print multiple element in one line
+
+x="Python"
+y="is"
+z="awesome"
+print(x,y,z)
+# or
+print(x+y+z)
+
+# remember if one is integer and one is string
+# then we cant use + operator
+
+u=5
+i="john"
+print(u+i) # error
