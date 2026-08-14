49787797302cd4c699920925051ec704a93f8ee1
Nitin Raj <nitinrana9060888018@gmail.com>
2026-08-15 02:53:39 +0530

    escape_character

---
+++ Escape_Character.py
@@ -0,0 +1,16 @@
+# to insert characters that are illegal in string use
+# \
+
+#txt="we are so called "vikings" from the north" this we cant able to do 
+
+# to do we have to use espace character \
+#                    \        \"
+txt="we are so called \"vikings\" from the north"
+print(txt)
+
+# single quote use \
+print("'I\' am nitin")
+
+print("hello \n World")
+
+print("this is \t jack") #\t
