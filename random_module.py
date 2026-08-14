76c1c7d557a57b5bae0711d4d9b0dc3df0988a54
Nitin Raj <nitinrana9060888018@gmail.com>
2026-08-15 01:09:06 +0530

    random generator

---
+++ random_module.py
@@ -0,0 +1,11 @@
+#to generate random varibale 
+# we use random import module
+import random # module
+
+# random.randrange(a,b) (b is not included)
+x=random.randrange(1,10)
+print(x)
+
+# random.randint(a,b) (b is included)
+y=random.randint(1,10)
+print(y)
