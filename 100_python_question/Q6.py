81e25cb42a810cba9ab17e3bb15c606503bbd0e9
Nitin Raj <nitinrana9060888018@gmail.com>
2026-08-18 01:07:02 +0530

    Question 6

---
+++ 100_python_question/Q6.py
@@ -0,0 +1,7 @@
+# 6. Write a program that will tell whether the number entered by the user is odd or even.
+x=int(input("Enter x: "))
+#y=int(input("Enter y: "))
+if(x%2==0):
+    print("even")
+else:
+    print("odd")
