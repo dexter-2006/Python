744a7b50a550db6a125565de1dc60122e35d1a6f
Nitin Raj <nitinrana9060888018@gmail.com>
2026-08-17 18:08:27 +0530

    Question 3

---
+++ 100_python_question/Q3.py
@@ -0,0 +1,12 @@
+# 3. User will input (2numbers). Write a program to swap the numbers
+
+x=int(input("enter num x : "))
+y=int(input("enter num y : "))
+z=0 #temp variable
+z=x
+x=y # now exchange
+y=z #update
+print(x)
+print(y)
+
+# Method 2
