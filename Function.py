599fd0bb8b944d55758a7b62e120179d206399bd
Nitin Raj <nitinrana9060888018@gmail.com>
2026-08-17 16:25:44 +0530

    Function too be continued

---
+++ Function.py
@@ -0,0 +1,24 @@
+# functions is the block of code which only runs when we call
+# it is used to make code short reusable
+
+# to create function in python we use def keyword
+def myfunction():
+    print("hello function")
+
+myfunction() # calling of function
+
+# just remeber function name not start with number
+
+def conert(f):
+    return (f-32)*(5/9)
+print(conert(77))
+print(f"degree is := {conert(90)}")
+
+# to send data back to funtion use return() keyword
+
+# diffrence betweem parameter and argument
+ # parameter := myfun(parameter) 
+ # myfun(value) argument
+
+ # we have do function in details later
+
