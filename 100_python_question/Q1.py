823916ed924d4e64e81d43e562b34a895c1e9582
Nitin Raj <nitinrana9060888018@gmail.com>
2026-08-17 17:39:24 +0530

    Question 1

--- 100_python_question/Q1.py
+++ 100_python_question/Q1.py
@@ -1,1 +1,13 @@
-# try
+#1. User will input (3ages).Find the oldest one 
+x=int(input("Enter age of one:= "))
+y=int(input("Enter age of two:= "))
+z=int(input("Enter age of three:= "))
+
+if(x>y and x>z):
+    print(" x is oldest")
+elif(y>x and y>z):
+    print("y is oldest")
+elif(z>x and z>y):
+    print("z is oldest")
+else:
+    print("error")
